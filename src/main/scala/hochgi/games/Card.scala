78a700a01b11da99b214ea4c52be1e8136656853
Gilad Hoch <gilad.hoch@thomsonreuters.com>
2013-07-02 17:29:34 +0300

    poker problem...

---
+++ src/main/scala/hochgi/games/Card.scala
@@ -0,0 +1,26 @@
+package hochgi.games
+
+class Card(val rank: Int, val suit: Char) {
+
+  def this(card: String) = this(Card.cardRank(card(0)), card(1))
+  override def toString: String = Card.rankCard(rank) + " of " + {
+    suit match {
+      case 'C' => "Clubs"
+      case 'S' => "Spades"
+      case 'D' => "Diamonds"
+      case 'H' => "Hearts"
+    }
+  }
+}
+
+object Card {
+  private val cardRank = Map[Char, Int]('2' -> 2,
+    '3' -> 3, '4' -> 4, '5' -> 5, '6' -> 6, '7' -> 7,
+    '8' -> 8, '9' -> 9, 'T' -> 10, 'J' -> 11,
+    'Q' -> 12, 'K' -> 13, 'A' -> 14)
+
+  private val rankCard = Map[Int, String](2 -> "2",
+    3 -> "3", 4 -> "4", 5 -> "5", 6 -> "6", 7 -> "7",
+    8 -> "8", 9 -> "9", 10 -> "10", 11 -> "Jack",
+    12 -> "Queen", 13 -> "King", 14 -> "As")
+}
